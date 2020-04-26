8acbf963047992cc5ad2cf6ef5a7580c167ed1a9
muhammad <dinoganteng7700@gmail.com>
2020-04-26 11:52:25 +0800

    CaraLainMendefinisikkanArray

--- src/Arrray/Main.java
+++ src/Arrray/Main.java
@@ -3,9 +3,13 @@ package Arrray;
 public class Main {
     public static void main(String[] args) {
 
-        int nilaiArr[] = {40,55,63,17,22,68,89,97,89};
-        System.out.println(nilaiArr.length);
-        System.out.println(nilaiArr[0]);
-        System.out.println(nilaiArr[8]);
+        String mataKuliah[];
+        mataKuliah = new String[10];
+
+        mataKuliah[0] = "Pemrograman Berbasis Objek";
+        mataKuliah[1] = "Visual 2";
+        mataKuliah[2] = "Rekayasa Perangkat Lunak";
+        mataKuliah[3] = "Organisasi Komputer";
+        mataKuliah[4] = "Pemrograman Web 2";
     }
 }
